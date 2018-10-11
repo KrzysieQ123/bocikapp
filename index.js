0aa557d3d4c8b4cce80c7d798b16b4caa8d02069
KrzysieQ123 <krzysieq95.mail@gmail.com>
2018-10-12 01:31:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var yt = require("ytdl-core");
 var config = require("./config.json");
 var bot = new discord.Client();
 var clever = new cleverbot("Oe6fkgAQ0PYhtTHe", "m4LZjjD91R9rSj9fVzkFiWpmpeQqmpDi");
+var volumeMusic=15;
 clever.setNick("LsmBot");
 if(!fs.existsSync('./permissions.json')){
 	var perm={}
@@ -18,7 +19,7 @@ var perm=require('./permissions.json');
 function play(connection, msg){
 	var server = servers[msg.guild.id];
 	var stream = yt(server.queue[0], {filter: "audioonly"});
-	server.dispatcher = connection.playStream(stream, {seek:0,volume:1});
+	server.dispatcher = connection.playStream(stream, {seek:0,volume:volumeMusic/100});
 	server.dispatcher.on("end", function(){
 		server.queue.shift();
 		if(server.queue[0]){
@@ -109,16 +110,13 @@ bot.on("message", function(msg){
 		var text = input;
 		msg.delete();
 		if(!msg.member.voiceChannel){
-			msg.channel.send("Nie jesteś na kanale głosowym.");
-			return;
+			return msg.channel.send("Nie jesteś na kanale głosowym.");
 		}
 		if(!msg.guild.voiceConnection){
-			msg.channel.send("Nie ma mnie na kanale głosowym.");
-			return;
+			return msg.channel.send("Nie ma mnie na kanale głosowym.");
 		}
 		if(!text){
-			msg.channel.send("Podaj tekst.");
-			return;
+			return msg.channel.send("Podaj tekst.");
 		}else{
 			var wynik="";
 			for(var letter=0; letter<text.length; letter++){
@@ -130,7 +128,7 @@ bot.on("message", function(msg){
 				wynik+= ""+text[letter].toLowerCase()+"";
 			}
 			msg.member.voiceChannel.join().then(function(connection){
-				var dispatcher = connection.playStream('http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q='+wynik+'&tl=pl', {seek:0,volume:1});
+				var dispatcher = connection.playStream('http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q='+wynik+'&tl=pl', {seek:0,volume:volumeMusic/100});
 				dispatcher.on("end", function(){
 					return;
 				});
@@ -146,24 +144,23 @@ bot.on("message", function(msg){
                 msg.channel.send("Javascript: "+e);
             }
         }else{
-            msg.delete();
-            return;
+            return msg.delete();
         }
     }
 	if(cmd == `${config.prefix}join`){
 		msg.delete();
 		var channel = msg.member.voiceChannel;
 		if(msg.guild.voiceConnection){
-			msg.channel.send("Jestem już na kanale!");
+			return msg.channel.send("Jestem już na kanale!");
 		}else{
 			if(channel){
 			channel.join().then(function(connection){
 				console.log(`Joined to voice channel on server ${msg.guild.id}.`);
 				msg.channel.send("**Aby dodać jakiś utwór do playlisty użyj komendy: __!play__ oraz podaj link.\nAby pominąć utwór użyj komendy: __!skip__.\nAby zakończyć odtwarzanie użyj komendy: __!leave__.**");
+				if(!servers[msg.guild.id]) servers[msg.guild.id]={queue:[]}
 			});
 			}else{
-				msg.channel.send('Nie jesteś na kanale głosowym.');
-				return;
+				return msg.channel.send('Nie jesteś na kanale głosowym.');
 			}
 		}
 	}
@@ -173,17 +170,14 @@ bot.on("message", function(msg){
 		var ytlink = link.match(checkLink);
 		msg.delete();
 		if(!msg.member.voiceChannel){
-			msg.channel.send("Nie jesteś na kanale głosowym.");
-			return;
+			return msg.channel.send("Nie jesteś na kanale głosowym.");
 		}
 		if(!msg.guild.voiceConnection){
-			msg.channel.send("Nie ma mnie na kanale głosowym.");
-			return;
+			return msg.channel.send("Nie ma mnie na kanale głosowym.");
 		}
 		if(!servers[msg.guild.id]) servers[msg.guild.id]={queue:[]}
 		if(!ytlink){
-			msg.channel.send("Podaj link.");
-			return;
+			return msg.channel.send("Podaj link.");
 		}else{
 			var server = servers[msg.guild.id];
 			if(!server.queue[0]){
@@ -206,13 +200,60 @@ bot.on("message", function(msg){
 		}
 	}
 	if(cmd == `${config.prefix}skip`){
-		var server = servers[msg.guild.id];
-		if(server.dispatcher) server.dispatcher.end();
+		msg.delete();
+		if(!msg.member.voiceChannel){
+			return msg.channel.send("Nie jesteś na kanale głosowym.");
+		}
+		if(!msg.guild.voiceConnection){
+			return msg.channel.send("Nie ma mnie na kanale głosowym.");
+		}else{
+			var server = servers[msg.guild.id];
+			if(!server.queue[0]){
+				msg.channel.send("Aktualnie nie jest odtwarzany żaden utwór.");
+			}else{
+				yt.getInfo(server.queue[0], (err, info)=>{
+					if(err) console.log(err);
+					msg.channel.send("Pominięto utwór **"+info.title+"**");
+					if(server.dispatcher) server.dispatcher.end();
+				});
+			}
+		}
+	}
+	if(cmd == `${config.prefix}volume`){
+		msg.delete();
+		if(!msg.member.voiceChannel){
+			return msg.channel.send("Nie jesteś na kanale głosowym.");
+		}
+		if(!msg.guild.voiceConnection){	
+			return msg.channel.send("Nie ma mnie na kanale głosowym.");
+		}else{
+			if(input){
+				if(isNaN(input) && input<1 || input>100){
+					return msg.channel.send("Aby zmieninić głośność użyj: !volume 1-100.");
+				}else{
+					var server = servers[msg.guild.id];
+					if(!server.dispatcher){
+						volumeMusic=input;
+						msg.channel.send(`Głośność została zmieniona na: **${input}**`);
+					}else{
+						server.dispatcher.setVolume((input / 100));
+						msg.channel.send(`Głośność została zmieniona na: **${input}**`);
+					}
+				}
+			}else{
+				return msg.channel.send("Aby zmieninić głośność użyj: !volume 1-100.");
+			}
+		}
 	}
 	if(cmd == `${config.prefix}leave`){
 		msg.delete();
 		var server = servers[msg.guild.id];
-		if(msg.guild.voiceConnection){
+		if(!msg.member.voiceChannel){
+			return msg.channel.send("Nie jesteś na kanale głosowym.");
+		}
+		if(!msg.guild.voiceConnection){
+			return msg.channel.send("Nie ma mnie na kanale głosowym.");
+		}else{
 			if(!server) server = {queue: []};
 			if(server.queue[0]){
 				for(var i = server.queue.length - 1; i >= 0; i--){
@@ -224,4 +265,4 @@ bot.on("message", function(msg){
 		}
 	}
 });
-bot.login(process.env.BOT_TOKEN);
+bot.login("NDMwNDkwMjQwMTY0Mjk4Nzc0.DprK5Q.e-kci0A3AoV_sW1Yv1PBWnpMV38");
